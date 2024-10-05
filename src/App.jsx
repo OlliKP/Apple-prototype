475526e0f59401f2d1ebd628c8201de98f256344
Oliver Krumbæk Perving <Ollikp@gmail.com>
2024-10-05 23:07:10 +0200

    Navbar component styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
 const App = () => {
   return <main className="bg-black">
-    
+    <Navbar />
+    <Hero />
+    <Highlights />
   </main>;
 };
 
